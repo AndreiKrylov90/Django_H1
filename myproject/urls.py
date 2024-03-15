404f6f6e4519f4b6a799711b9721188f177a75c7
Andrei.Krylov <dronovoy@yandex.ru>
2024-03-15 17:04:00 +0300

    images are displayed

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from myapp3.views import index
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +28,4 @@ urlpatterns = [
     path('', index),
     path('h2/', include('homework2.urls')),
     path('les4/', include('myapp4.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
